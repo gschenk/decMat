52ae66da3226558f0337c8840cfb83facda64b6b
gerald schenk <gschenk@gmx.net>
2020-01-13 23:40:10 +0100

    object for decission matrix, rm duplicate file

--- index.js
+++ index.js
@@ -4,8 +4,25 @@ import readYmlData from './data.js';
 
 
 const inFilePath = './example.yaml';
-const YmlDoc = readYmlData(inFilePath);
-const ymlKeys = Object.keys(YmlDoc);
+
+// constructor
+function DecisionObject(o) {
+  const cats = Object.keys(o);
+  this.cats = cats;
+
+  // access values by number of n of category.
+  const entries = (n) => o[cats[n]];
+  this.entries = entries;
+
+  this.dimX = Object.keys(o).length;
+  // the largest number of values assigned to any key cat of object o.
+  this.dimY = Math.max(...cats.map((k) => o[k].length));
+}
+
+const YmlDoc = new DecisionObject(readYmlData(inFilePath));
+
+
+const ymlKeys = YmlDoc.cats;
 
 console.log(YmlDoc);
 
