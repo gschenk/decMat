1bad13b8226c2be39ebe70085885077a682c3774
gerald schenk <gschenk@gmx.net>
2020-01-20 17:48:11 +0100

    moves matrix dimensions M, N to dim object

--- src/core.js
+++ src/core.js
@@ -5,9 +5,11 @@
 function DecisionMatrixO(data) {
   const cats = Object.keys(data);
 
+  // M rows
   // dimM is the largest number of values assigned to any key cat of object o.
   const dimN = cats.length;
 
+  // N columns
   // function to get max number of entries per column
   // dimM :: Object => Integer
   const dimM = (o) => Math.max(...cats.map((k) => o[k].length));
@@ -24,11 +26,9 @@ function DecisionMatrixO(data) {
   // this.cats :: [String]
   this.cats = cats;
 
-  // this.dimM :: Integer
-  this.dimN = dimN;
-
-  // this.dimM:: Integer
-  this.dimM = dimM(data);
+  // capital M, N for Integers; maths convention > JS convention
+  // this.dimM :: { Integer, Integer }
+  this.dim = { M: dimM(data), N: dimN };
 
   // this zeroToM :: [Integer]
   this.zeroToM = zeroToI(dimM);
